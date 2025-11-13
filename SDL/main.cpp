9710610c49fb66961099d6dee012ca98355a29b1
chiluncc <1431530893@qq.com>
2025-11-13 20:11:48 +0800

    color change screen

--- SDL/main.cpp
+++ SDL/main.cpp
@@ -22,11 +22,20 @@ int main(int argc, char* args) {
 	bool quit = false;
 	SDL_Event event;
 	SDL_zero(event);
+	Uint32 color = 0XFFFF;
 	while (!quit) {
+		Uint8 upBits = (color & 0XFF000000) >> 24;
+		color = (color << 8) | upBits;
+		Uint32 curColor = SDL_MapSurfaceRGB(surface, (color & 0XFF0000) >> 16, (color & 0XFF00) >> 8, color & 0XFF);
+		SDL_FillSurfaceRect(surface, nullptr, curColor);
 		while (SDL_PollEvent(&event)) {
 			if (event.type == SDL_EVENT_QUIT) quit = true;
 		}
-		SDL_Delay(8);
+		SDL_UpdateWindowSurface(window);
+		SDL_Delay(50);
 	}
+	SDL_DestroyWindow(window);
+	SDL_DestroySurface(surface);
+	SDL_Quit();
 	return 0;
 }
